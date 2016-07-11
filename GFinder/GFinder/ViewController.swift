aadafbd0288c6910e03734712536f284482236b9
Raghavendra <raghavendra@MikkilineniRaghavendra.local>
2016-07-11 22:08:53 +0530

    created project with swift language and its first commit.

---
+++ GFinder/GFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GFinder
+//
+//  Created by Raghavendra on 11/07/16.
+//  Copyright © 2016 loftySoftwareSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
