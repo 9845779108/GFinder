bc2afa672698d9ec11d21a9c0c3ab00b7de14f1d
Raghavendra <raghavendra@MikkilineniRaghavendra.local>
2016-07-11 22:30:40 +0530

    integrated the pods to the project .

---
+++ GFinder/Pods/Target Support Files/Pods-GFinder/Pods-GFinder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GFinder : NSObject
+@end
+@implementation PodsDummy_Pods_GFinder
+@end
---
+++ GFinder/Pods/Target Support Files/Pods-GFinder/Pods-GFinder-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GFinderVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GFinderVersionString[];
+
